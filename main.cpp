21c096cebf32cd90808ae0d99f41fa101222b60b
SCUT-CCNL <33176838+SCUT-CCNL@users.noreply.github.com>
2018-09-07 21:51:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/***
+    @author: Jiabing Fu, Bixin Ke, Shoubin Dong.
+    @date：2018.08.08
+    @institute: South China University of Technology
+    @Paper: Submitted to Bioinformatics.
+***/
+
 #include "lcqs.h"
 #include <stdexcept>
 #include <fstream>
