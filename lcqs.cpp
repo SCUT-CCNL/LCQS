1e22df22e9c70521ef28fd446a47010391adb067
SCUT-CCNL <33176838+SCUT-CCNL@users.noreply.github.com>
2018-09-07 23:58:39 +0800

    Update lcqs.cpp

--- lcqs.cpp
+++ lcqs.cpp
@@ -2,7 +2,6 @@
     @author: Jiabing Fu, Bixin Ke, Shoubin Dong.
     @date：2018.08.08
     @institute: South China University of Technology
-    @Paper: Submitted to Bioinformatics.
 ***/
 
 #include "lcqs.h"
